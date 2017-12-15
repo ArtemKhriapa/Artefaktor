450db65292753885edb2aa68de0372a814bc2f63
Artem <art.pzt@gmail.com>
2017-12-15 11:42:59 +0200

    fix OTC and settings

--- Artefaktor/apps/extraapps/OTC/models.py
+++ Artefaktor/apps/extraapps/OTC/models.py
@@ -3,13 +3,21 @@ import hashlib
 
 class OTC(models.Model):
     datetime = models.DateTimeField(auto_now_add = True)
-    otc = models.CharField(max_length = 64, blank = True,  unique = True)
+    otc = models.CharField(max_length = 64, blank = True)
     is_used = models.BooleanField(default = False)
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.otc = self.calculate(str(self.datetime))
+         super().__init__(*args, **kwargs)
+         self.otc = self.calculate(str(self.datetime))
+
+    # def save(self, *args, **kwargs):
+    #     self.otc = self.calculate(str(self.datetime))
+    #     super().save(self, *args, **kwargs)
 
     def calculate(self,salt):
          res = hashlib.sha256(salt.encode('utf-8'))
          return res.hexdigest()
+
+    def __str__(self):
+        return "ID: %s, Time: %s, OTC: %s, Used: %s" % \
+               (self.id, self.datetime, self.otc, self.is_used)
