4c0f02d8969a6dbb04a643b11b3f0c4347e8818b
yar4 <yaro93@live.ru>
2017-12-16 23:05:24 +0200

    Authorization

--- Artefaktor/apps/Authentification/models.py
+++ Artefaktor/apps/Authentification/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 # Create your models here.
 class SignupForm(UserCreationForm):
@@ -11,3 +12,6 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class Authorization(AuthenticationForm):
+    pass
