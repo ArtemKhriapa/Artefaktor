0bc96da521bad9f9c20dec49201bd01ce264f578
yar4 <yaro93@live.ru>
2017-12-16 23:00:42 +0200

    Auth model

---
+++ Artefaktor/apps/Authentification/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+# Create your models here.
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=500, help_text='Required')
+#as far as UserCreationForm was imported
+#we can do a sign up form with extra field email
+# https://docs.djangoproject.com/en/1.8/_modules/django/contrib/auth/forms/
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
