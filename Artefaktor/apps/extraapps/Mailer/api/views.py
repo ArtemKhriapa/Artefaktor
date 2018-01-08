6eafb9782439cf2fa0733faec2b9f6ca164aa350
yar4 <yaro93@live.ru>
2018-01-08 14:25:06 +0200

    Template change

--- Artefaktor/apps/extraapps/Mailer/api/views.py
+++ Artefaktor/apps/extraapps/Mailer/api/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.core.signals import request_finished
 from django.dispatch import receiver
 from apps.userauth import User
+import django.db.models.signals
+import django.template.loader
 
 # Create your views here.
 
-@receiver(registration_mail,sender=User )
-def mail_sender(sender, **kwargs):
+@receiver(post_save,sender=User )
+def mail_notification(sender, **kwargs):
+    get_template('Artefaktor/Templates/OTC_Check_Template.html')
     send_mail(
         'Please verify your account',
-        message=render_to_string('OTC_Check_Templates.html', {'foo': 'bar'}),
+        message=render_to_string('Artefaktor/Templates/OTC_Check_Template.html', {'foo': 'bar'}),
         to=User.user_email)
