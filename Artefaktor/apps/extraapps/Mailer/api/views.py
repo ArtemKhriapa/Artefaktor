afdbda266457aa0e0f0db324fd9811b045ba3450
yar4 <yaro93@live.ru>
2018-01-10 18:32:52 +0200

    Mailer

--- Artefaktor/apps/extraapps/Mailer/api/views.py
+++ Artefaktor/apps/extraapps/Mailer/api/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
-from django.core.signals import request_finished
+from django.core.mail import send_mail
+from Artefaktor.Artefaktor.apps.userauth.models import RegistrationTry
 from django.dispatch import receiver
-from apps.userauth import User
-import django.db.models.signals
-import django.template.loader
+from django.db.models.signals import post_save
+from django.template.loader import get_template, render_to_string
+from django.contrib.auth import User
 
-# Create your views here.
-
-@receiver(post_save,sender=User )
+@receiver(post_save,sender=RegistrationTry)
 def mail_notification(sender, **kwargs):
     get_template('Artefaktor/Templates/OTC_Check_Template.html')
     send_mail(
         'Please verify your account',
         message=render_to_string('Artefaktor/Templates/OTC_Check_Template.html', {'foo': 'bar'}),
-        to=User.user_email)
+        from_email= no_reply_art@outlook.com,
+        recipient_list=User.email)
